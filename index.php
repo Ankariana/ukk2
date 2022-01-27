736822300149076c9c0c237ab074c7c189432159
Ankariana <97925547+Ankariana@users.noreply.github.com>
2022-01-27 08:57:07 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
    
 </head>
+<?php 
+include("koneksi.php");
+include("konten/head.html");
+?>
 <body>
     <div class="contrainer">
-        
+<?php
+   include("konten/judul.html");
+   include("konten/nav.php");
+
+   include("atur-konten.php");
+?>
+   ?>
     </div>
 </body>
 </html>
